a57d2cb99a322a28b86454ad1fdfc1f8e22eaa54
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-03-16 20:33:04 +0900

    22/03/16 수업
    printf, 단축키, 전처리기

--- Client_C/Client_FirstProject/FirstProject.c
+++ Client_C/Client_FirstProject/FirstProject.c
@@ -1,7 +1,18 @@
+#include <stdio.h>
 
+// 주석 설정 단축키 : ctrl + k + c  ctrl + shift + slash
+// 주석 해제 단축기 : ctrl + k + u
+// 다중라인 선택 단축키 : Alt + Shift 한 채로 키보드 화살표 방향키 또는 마우스 클릭 / 드래그
+
+// /* 와 */ 사이 내용 컴파일러 시 무시
+
+// 전처리기 클릭하고 F12 누르면 해당 전처리기 소스파일 세부내용 볼 수 있음
 
 
 int main(void)
 {
 
+	printf("%d", sizeof(1));
+
+	return 0;
 }
