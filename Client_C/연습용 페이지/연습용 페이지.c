8923e14cc69eb134e9ef85f161ac6d76b15b402c
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-04-05 16:10:43 +0900

    ??

--- Client_C/연습용 페이지/연습용 페이지.c
+++ Client_C/연습용 페이지/연습용 페이지.c
@@ -5,38 +5,9 @@
 
 int main()
 {
-<<<<<<< HEAD
-    struct student studentA;
-
-    struct StudentList {
-        struct student students[2];
-        int num;
-    } studentList;)
-
-    strcpy(studentA.name, "Likee");
-    studentA.english = 80;
-    studentA.korean = 60;
-    studentA.math = 40;
-    studentA.average = (double)(studentA.english + studentA.korean + studentA.math + studentA.math) / 3.0;
-
-    struct student studentB = { {100}, {55}, {32}, {"Jerry"}, {(double)(55 + 65 + 32) / 3.0}
-    };
-
-    struct student* studentAP;
-    struct student* studentBP;
-=======
-    int** a;
-    int* b;
-    int c = 10;
->>>>>>> 474e150c780cf37254afec6109554dad4ff9cbb4
-
-    a = &b;
-    b = &c;
-
-    printf("%d", b[0]);
-
-
-
-
+    char a[100] = "asdfasdfasd";
+    char* p = "hello";
+    strcpy(a[0], '5');
+    printf("%s\n", a);
     return 0;
 }
